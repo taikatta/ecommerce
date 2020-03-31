b2c44674bdff94128268523b5f8c1690d69c93a1
Boglarka Barabas <taikatta@gmail.com>
2020-03-31 22:26:28 +0100

    django updated to 1.11.28

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ecommerce-bo.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "ecommerce-bo.herokuapp.com"]
 
 
 # Application definition
